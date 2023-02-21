e0417fd7109d3034566b502c2b82cf810f7ec0cc
abethel912 <abethel180@gmail.com>
2023-02-20 20:44:23 -0500

    Route testing

--- firstapp/views.py
+++ firstapp/views.py
@@ -3,5 +3,18 @@ from django.http import JsonResponse
 from django.views import View
 # Create your views here.
 
+
 class FirstView(View):
-    pass
+    def get(self, request):
+        return JsonResponse({"hello": "world-get"})
+
+    def post(self, request):
+        return JsonResponse({"hello": "world-post"})
+
+    def put(self, request):
+        return JsonResponse({"hello": "world-put"})
+
+    def delete(self, request):
+        return JsonResponse({"hello": "world-delete"})
+def functionview(request):
+    return JsonResponse({"hey":"it works"})
--- firstdjango/urls.py
+++ firstdjango/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from firstapp.views import FirstView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("first/", FirstView.as_view()),
 ]
