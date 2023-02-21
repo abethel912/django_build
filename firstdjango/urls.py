33af74f0b9dfc96dae4d118b4e6587ae1e02a402
abethel912 <abethel180@gmail.com>
2023-02-21 14:08:49 -0500

    Configured URL for template route

--- firstdjango/urls.py
+++ firstdjango/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from firstapp.views import FirstView, functionview
+from firstapp.views import FirstView, functionview, templateview
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("first/", FirstView.as_view()),
-    path("f/", functionview)
+    path("f/", functionview),
+    path("template/", templateview)
 ]
